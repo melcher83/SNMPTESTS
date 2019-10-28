09274e861f958c20d47b85fab8cf59b0a23eabdd
JonathanMelcher <melcher83@gmail.com>
2019-10-28 12:50:29 -0500

    made into function

--- snmp.py
+++ snmp.py
@@ -1,18 +1,22 @@
 from pysnmp.hlapi import *
 
-iterator = getCmd(SnmpEngine(),
+def SNMP_V2MIB_GET(var,instance):
+
+    iterator = getCmd(SnmpEngine(),
                   CommunityData('public'),
-                  UdpTransportTarget(('127.0.0.1', 161)),
+                  UdpTransportTarget(('192.168.127.52', 161)),
                   ContextData(),
-                  ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)))
+                  ObjectType(ObjectIdentity('SNMPv2-MIB', var, instance)))
 
-errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+    errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
 
-if errorIndication:  # SNMP engine errors
-    print(errorIndication)
-else:
-    if errorStatus:  # SNMP agent errors
-        print('%s at %s' % (errorStatus.prettyPrint(), varBinds[int(errorIndex)-1] if errorIndex else '?'))
+    if errorIndication:  # SNMP engine errors
+        print(errorIndication)
     else:
-        for varBind in varBinds:  # SNMP response contents
-            print(' = '.join([x.prettyPrint() for x in varBind]))
+        if errorStatus:  # SNMP agent errors
+            print('%s at %s' % (errorStatus.prettyPrint(), varBinds[int(errorIndex)-1] if errorIndex else '?'))
+        else:
+            for varBind in varBinds:  # SNMP response contents
+                print(' = '.join([x.prettyPrint() for x in varBind]))
+
+SNMP_V2MIB_GET('sysDescr',0)
