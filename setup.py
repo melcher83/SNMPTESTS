6258d9161d0cfc1ad11cd1bedb4d55aa36da533c
JonathanMelcher <melcher83@gmail.com>
2019-10-30 11:29:35 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SNMPTESTS',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/melcher83/SNMPTESTS',
+    license='GPL-2.0',
+    author='JonathanMelcher',
+    author_email='jonathan@vlan404notfound.com',
+    description=''
+)
